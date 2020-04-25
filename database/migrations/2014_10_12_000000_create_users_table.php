7ec47307d2b9d75ee01e38a3c8aa1cb6aa605e25
Ishara Dilshan <isharadilshandev@gmail.com>
2020-04-26 04:07:12 +0530

    fixed can't merge pull request

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user');
-            $table->boolean('verified')->default(0);
+            $table->string('role');
+            $table->boolean('verified');
             $table->rememberToken();
             $table->timestamps();
         });
